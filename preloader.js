d1b6a71a073ff6d14230fbee771cd6cad2731a1b
Lokwei Hezron <122450611+bugzorc@users.noreply.github.com>
2025-04-22 19:31:56 +0300

    Delete preloader.js

--- preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    var preloader = document.getElementById('preloader');
-    var content = document.getElementById('content');
-
-    preloader.style.display = 'none'; // Hide the preloader 
-});
